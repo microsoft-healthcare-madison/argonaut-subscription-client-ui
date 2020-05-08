ea81d8631830bbdb528403ac7a8e9ae481a277d2
Gino Canessa <ginocanessa@gmail.com>
2020-05-08 17:12:03 -0500

    Fixed issues from merge.

--- src/models/fhir_r4_selected.ts
+++ src/models/fhir_r4_selected.ts
@@ -543,15 +543,6 @@ export enum SubscriptionStatusStatusCodes {
 	OFF = "off",
 }
 /**
- * Code Values for the SubscriptionTopic.status field
- */
-export enum SubscriptionTopicStatusCodes {
-	DRAFT = "draft",
-	ACTIVE = "active",
-	RETIRED = "retired",
-	UNKNOWN = "unknown",
-}
-/**
  * Code Values for the SubscriptionTopicCanFilterBy.searchModifier field
  */
 export enum SubscriptionTopicCanFilterBySearchModifierCodes {
@@ -603,20 +594,6 @@ export enum SubscriptionTopicCanFilterByMatchTypeCodes {
 	NOT_IN = "not-in",
 	ABOVE = "above",
 	BELOW = "below",
-}
-/**
- * Code Values for the SubscriptionTopicResourceTrigger.methodCriteria field
- */
-export enum SubscriptionTopicResourceTriggerMethodCriteriaCodes {
-	CREATE = "create",
-	UPDATE = "update",
-	DELETE = "delete",
-}
-/**
- * Code Values for the SubscriptionTopicResourceTrigger.resourceType field
- */
-export enum SubscriptionTopicResourceTriggerResourceTypeCodes {
-	CANDIDATE = "Candidate",
 }
 /**
  * Code Values for the TimingRepeat.dayOfWeek field
